2cdabb521ae80c448ee26b3003956e0e03154933
Volodymur Honcharuk <volodymyrmain@gmail.com>
2025-07-12 20:58:03 +0300

    chore: add IMask library and implement mask for Ukrainian phone number

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 import '../styles/main.scss'
 import Search from './Search.js'
 import ModalCollection from './AuthModal.js'
+import InputMaskCollection from './InputMask.js'
 
 new Search()
 new ModalCollection()
+new InputMaskCollection()
