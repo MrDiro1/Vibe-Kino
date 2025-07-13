1dec327961e5a8195e701f86fdd925ff0378041b
Volodymur Honcharuk <volodymyrmain@gmail.com>
2025-07-13 15:35:59 +0300

    feat: integrate Swiper.js and implement slider logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ import Search from './Search.js'
 import ModalCollection from './AuthModal.js'
 import InputMaskCollection from './InputMask.js'
 import { initGoogleAuth } from './google-auth.js'
+import SliderCollection from './Slider.js'
 
 const CLIENT_ID =
 	'338113015991-8lidjllokbu1dkbeq5p166c02lgjc4mf.apps.googleusercontent.com'
@@ -11,3 +12,4 @@ new Search()
 new ModalCollection()
 new InputMaskCollection()
 initGoogleAuth(CLIENT_ID)
+new SliderCollection()
