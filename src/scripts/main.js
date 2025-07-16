3e7fe4d00946db85c9cb6229dc1c586b558682fd
Volodymur Honcharuk <volodymyrmain@gmail.com>
2025-07-16 14:20:27 +0300

    hide client-id

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,7 @@ import ModalCollection from './AuthModal.js'
 import InputMaskCollection from './InputMask.js'
 import { initGoogleAuth } from './google-auth.js'
 
-const CLIENT_ID =
-	'338113015991-8lidjllokbu1dkbeq5p166c02lgjc4mf.apps.googleusercontent.com'
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 new Search()
 new ModalCollection()
